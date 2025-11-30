b356d2278502865cfa0f77ab8bedfcf741f65592
blankkywired <blankky@protonmail.com>
2025-11-30 14:41:36 -0300

    I've no idea

--- list.py
+++ list.py
@@ -1,19 +1,22 @@
-myList = [":", ")" , ":", ")" , ":" , ")"]
+myList = [":", ")" , ":", "(" ]
 #To-do
 #Agrupar os characteres em pares caso o tamanho da lista seja um valor par
 #Cada par de caracteres deve ter um indice dentro da lista
 #Objetivo --> [":)", ":)", ":)"]
 
-
+#Agrupando caracteres
 emoteList = []
 a = ""
 b = []
 if len(myList) % 2 == 0:
     for i in myList:
         if myList.index(i) % 2 == 0: #Compara o numero do indice para ver se é um valor par
-            b.append(myList[myList.index(i)])
+            a =  myList[myList.index(i)]
             
-print(b)
-            
+            a =  a + myList[myList.index(i) - 1]
+            b.append(a)
 
+
+print(a)
+print(b)
 
