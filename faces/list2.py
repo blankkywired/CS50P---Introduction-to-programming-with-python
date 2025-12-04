b43bfbbe812a092e8e3127860c2a9d7c07746c20
blankkywired <blankky@protonmail.com>
2025-12-03 22:40:53 -0300

    This sh*t dont work holy fck

--- faces/list2.py
+++ faces/list2.py
@@ -1,6 +1,38 @@
-user_input = input(': ')
+#Recriar função split em python
+texto = input(': ')
+#print(texto.split())
 
-characters = []
 
-for character in user_input:
-    
+#Mostrando os indices de cada espço em branco
+splitInList = []
+indexChar = -1 #Contagem para exibir o indice contido em cada parte
+#ocorrenciasEspaco = 0
+for char in texto:
+    if char == " ":
+        splitInList.append(char)
+        indexChar += 1
+        print("Espaco apareceu no indice:" , indexChar)
+
+    else:
+        splitInList.append(char)
+        indexChar += 1
+#print(splitInList)
+#print(ocorrenciasEspaco)
+
+#Unir characteres
+indexCharacter = 0
+wordsList = []
+
+word = ""
+
+while indexCharacter <= splitInList.index(splitInList[-1]):
+    print(splitInList[indexCharacter])
+    if splitInList[indexCharacter] == " ":
+        word = ""
+        wordsList.append(word)
+    else:
+        word += splitInList[indexCharacter] #Vai adicionando cada letra para a string 
+        if indexCharacter == splitInList.index(splitInList[-1]):
+            wordsList.append(word)
+    indexCharacter += 1
+print(wordsList)
