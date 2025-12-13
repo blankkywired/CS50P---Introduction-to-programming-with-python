874c6c20745c0db238a1736e3f0141faef2e1545
blankkywired <blankky@protonmail.com>
2025-12-13 12:48:28 -0300

    Added comments

--- faces/faces.py
+++ faces/faces.py
@@ -1,9 +1,11 @@
 name = input()
 
 def faces(input):
-        
-    x = input.replace(":)", "🙂").replace(":(", "🙁")
+    x = input.replace(":)", "🙂")
+    x = x.replace(":(", "🙁")
+
+    #---- Other option ---#
+    #x = input.replace(":)", "🙂").replace.(":(", "🙁") ---> #Passing replace mthod twice 
     return x
-    
-        
+
 print(faces(name))
