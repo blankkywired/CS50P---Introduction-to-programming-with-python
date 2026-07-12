29c11b4c40c98c784e44e4b082b7e5833e8c8e30
blankkywired <blankky@protonmail.com>
2026-07-12 20:07:32 -0300

    Adde file meal.py

--- Week-1/Meal Time/meal.py
+++ Week-1/Meal Time/meal.py
@@ -1,7 +1,9 @@
-time_convert = input('What time is it?: ')
+
 
 def main():
-   HOUR = convert(time_convert)
+   global user_time
+   user_time = input('What time is it? ')
+   HOUR = convert(user_time)
 
    if HOUR >= 7 and HOUR <= 8:
        print('breakfast time')
@@ -14,20 +16,21 @@ def main():
 
 def convert(time):
     global time_convert
-    global list_char
-    time_convert = time_convert.split(":")
-    print(time_convert)
+    #Dividindo os caracteres da entrada do usuario da variavel user_time e guardando em uma lista
+    time_convert = time.split(":")
+    #print(time_convert) #---> Exibe uma lista de cada par de numeros separados pelo :
     hour = 0
     for number in time_convert:
         number = int(number)
         #print(number)
         if number == int(time_convert[0]):
             minutes = number * 60
-        elif number == int(time_convert[1]):
+        else:
             minutes += int(time_convert[1])
     hour = minutes / 60
     hour = round(hour, 2)
-    print(hour)
+    #print(hour)
     return hour
 if __name__ == "__main__":
     main()
+
